8f276ea63cd9819e4439c6e22b20c8f1e853bd53
Huijie Qiao <huijieqiao@Huijies-Mac-Pro.local>
2019-08-23 21:11:54 +0800

    20190823-3

--- add_country_coastline_provice_gbif.r
+++ add_country_coastline_provice_gbif.r
@@ -3,7 +3,7 @@ setwd("~/Experiments/IUCN_FIX/Script/iucn_fix")
 
 
 if (F){
-  df<-readRDS("../../Data/occ_without_NA_coordinate/Aves.RData")
+  df<-readRDS("/Volumes/Disk2/Experiments/Huijie/Data/occ_without_NA_coordinate/GBIF/Aves.RData")
   colnames(df)[4]<-'order'
   df<-df[which((!is.na(df$decimalLongitude))&(!is.na(df$decimalLatitude))),]
   head(df[which(is.na(df$decimalLongitude)),])
@@ -13,25 +13,37 @@ if (F){
   points_eck4<-spTransform(points, CRS="+proj=eck4 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
   df_eck4<-data.frame(points_eck4)
   colnames(df_eck4)[9:10]<-c("lon_eck4", "lat_eck4")
-  saveRDS(df_eck4, "../../Data/occ_without_NA_coordinate/Aves_with_eck4.RData")
+  saveRDS(df_eck4, "Aves_with_eck4.RData")
   
 }
+
+df<-readRDS("../../Data/occ_without_NA_coordinate/Mammalia_with_eck4.RData")
 country<-raster("../../Raster/country_boundaries_eck4.tif")
 coastline<-raster("../../Raster/coastal_boundaries_eck4.tif")
-province<-raster("../../Raster/province_b1_eck4.tif")
+province<-raster("../../Raster/province_eck4.tif")
 bio1<-raster("../../Raster/Bioclim2.0/500m/bio01.tif")
-f<-files[1]
-for (f in files){
-  print(f)
-  if (file.exists(sprintf("%s/%s", target, f))){
+realm<-raster("../../Raster/Biogeographic_realms_clip_eck4.tif")
+
+species<-unique(df$species)
+sp<-species[1]
+
+target<-"../../Data/GBIF_More_Data/Mammals/%s.rda"
+i=1
+for (i in c(1:length(species))){
+  sp<-species[i]
+  print(sp)
+  
+  file<-sprintf(target, gsub(" ", "_", sp))
+  if (file.exists(file)){
     next()
   }
-  saveRDS(NA, file=sprintf("%s/%s", target, f))
-  df<-readRDS(sprintf("%s/%s", folder, f))
-  df$country<-extract(country, df[, c("x", "y")])
-  df$coastline<-extract(coastline, df[, c("x", "y")])
-  df$province<-extract(province, df[, c("x", "y")])
+  saveRDS(NA, file=file)
+  item<-df[which(df$species==sp),]
+  item$country<-extract(country, item[, c("lon_eck4", "lat_eck4")])
+  item$coastline<-extract(coastline, item[, c("lon_eck4", "lat_eck4")])
+  item$province<-extract(province, item[, c("lon_eck4", "lat_eck4")])
+  item$bio1<-extract(bio1, item[, c("lon_eck4", "lat_eck4")])
+  item$realm<-extract(realm, item[, c("lon_eck4", "lat_eck4")])
   
-  df$bio1<-extract(bio1, df[, c("x", "y")])
-  saveRDS(df, file=sprintf("%s/%s", target, f))
+  saveRDS(item, file=file)
 }
