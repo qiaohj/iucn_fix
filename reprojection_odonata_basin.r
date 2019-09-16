72c155f5baab3d6a7e0916c7fc9ca5338a3660fe
Huijie Qiao <huijieqiao@Huijies-Mac-Pro.local>
2019-09-16 11:29:08 +0800

    20190916

--- reprojection_odonata_basin.r
+++ reprojection_odonata_basin.r
@@ -1,16 +1,29 @@
 library("raster")
-setwd("~/Experiments/IUCN_FIX/Script/iucn_fix")
-r<-raster("../../Raster/Odonata_Basin/obonata_basin.tif")
+setwd("/Volumes/Disk2/Experiments/IUCN_FIX/Script/iucn_fix")
+
+if (F){
+  r<-raster("../../Raster/Odonata_Basin/obonata_basin.tif")
+  mask<-raster("../../Raster/Bioclim2.0/500m/bio01.tif")
+  print("reproject")
+  rr<-projectRaster(r, crs=crs(mask), res=res(mask), method="ngb")
+  res(rr)
+  extent(rr)
+  print("write reproject")
+  writeRaster(rr, "../../Raster/Odonata_basin_overlap_eck4.tif", overwrite=T)
+  
+  print("buffer")
+  rr_buffer<-buffer(rr, width=500)
+  
+  print("write buffer")
+  writeRaster(rr_buffer, "../../Raster/Odonata_basin_overlap_eck4_500_buffer.tif", overwrite=T)
+}
+library("rgdal")
+#country_boundaries
+sp_df<-readOGR("../../raw_from_Alice/ob", "od_boundary")
 mask<-raster("../../Raster/Bioclim2.0/500m/bio01.tif")
-print("reproject")
-rr<-projectRaster(r, crs=crs(mask), res=res(mask), method="ngb")
-res(rr)
-extent(rr)
-print("write reproject")
-writeRaster(rr, "../../Raster/Odonata_basin_overlap_eck4.tif", overwrite=T)
+sp_df_eck4<-spTransform(sp_df, CRS=crs(mask))
+writeOGR(obj=sp_df_eck4, dsn="../../Shape/polygon", layer="od_boundary_buffer_eck4", driver="ESRI Shapefile")
 
-print("buffer")
-rr_buffer<-buffer(rr, width=500)
-
-print("write buffer")
-writeRaster(rr_buffer, "../../Raster/Odonata_basin_overlap_eck4_500_buffer.tif", overwrite=T)
+extent(mask)<-extent(sp_df_eck4)
+rp <- rasterize(sp_df_eck4, mask)
+writeRaster(rp, "../../Raster/od_boundary_buffer_eck4.tif", overwrite=T)
